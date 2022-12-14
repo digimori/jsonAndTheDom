57f58f65aa293a6d61fc7fe1c37dcbbc91408219
digimori <hanaorikuu@gmail.com>
2022-12-14 20:42:25 +0000

    practice stuff

---
+++ ajax-test/main.js
@@ -0,0 +1,29 @@
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+var data;
+xhr.open("GET", baseURL + type + "/");
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        cb(JSON.parse(this.responseText));
+    }
+};
+};
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
+
+function writeToDocument(type) {
+getData(type, function(data) {
+    document.getElementById('data').innerHTML = data;
+})
+}
+
+
+
